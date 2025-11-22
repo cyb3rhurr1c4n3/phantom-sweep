b7acf7c6af6909a287bf496edb85c08f5a17401a
cyb3rhurr1c4n3 <vobao8682@gmail.com>
2025-11-22 15:28:33 +0700

    Revert "[output] Prototype of JSON output"
    This reverts commit 03d26211a5ae8955d53eae10a28dd3c9e2b84623.

--- network_probe/plugins/Output/xml_reporter.py
+++ network_probe/plugins/Output/xml_reporter.py
@@ -1,9 +1,7 @@
 from typing import Dict
 from network_probe.plugins.base_plugin import BaseReport
-from dicttoxml import dicttoxml
+
 
 class XmlReporter(BaseReport):
     def save(self, results: Dict[str,any],filename: str):
-        # Tạm thời như v đi
-        if not filename.endswith(".xml"): filename += ".xml"
-        pass # ...idk
+        pass
